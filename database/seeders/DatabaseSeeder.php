1986909ed9d32e28706efcad0c39a412f19cfac7
luzumi <corataca@gmail.com>
2022-12-14 10:44:29 +0100

    Seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,6 +27,7 @@ class DatabaseSeeder extends Seeder
              'telefon' => fake()->phoneNumber(),
              'email_verified_at' => now(),
              'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+             'role' => 'admin',
              'remember_token' => Str::random(10),
          ]);
          ParkingSpot::factory()->create([
