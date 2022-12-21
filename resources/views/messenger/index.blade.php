9d77da88416a8f9c314c359a7357f3262accbeb8
luzumi <corataca@gmail.com>
2022-12-21 17:41:44 +0100

    Chatsystem finetuning

--- resources/views/messenger/index.blade.php
+++ resources/views/messenger/index.blade.php
@@ -57,7 +57,7 @@
     <script>
         function showThread(threadId) {
             $('#subject').css('display', 'flex');
-
+            $('#messages').empty();
             // Abrufen aller Nachrichten des Threads mit Hilfe von AJAX
             $.ajax({
                 url: '/messages/' + threadId,
